0fcff77255b83b9fcee30c65f37a227249dad000
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2023-03-01 12:34:45 +0200

    Format.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::Parser;
+use rand::distributions::WeightedIndex;
 use rand::seq::SliceRandom;
-use rand::{Rng, rngs::SmallRng, SeedableRng};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::PathBuf;
-use rand::distributions::WeightedIndex;
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -29,7 +29,12 @@ fn main() {
 
     let sequence = generate(options.length, &mut SmallRng::from_entropy());
     let output = BufWriter::new(File::create(options.sequence_out).unwrap());
-    write_fasta_record("random_reference", sequence.as_slice(), output, options.fasta_linewidth);
+    write_fasta_record(
+        "random_reference",
+        sequence.as_slice(),
+        output,
+        options.fasta_linewidth,
+    );
 
     if let Some(subsequence_length) = options.subsequence_length {
         let mut output = BufWriter::new(File::create(options.subsequence_out.unwrap()).unwrap());
@@ -38,8 +43,18 @@ fn main() {
         let subsequence = &sequence[sequence.len() - subsequence_length..];
         let reverse_complement = reverse_complement(subsequence);
 
-        write_fasta_record("random_contig", subsequence, &mut output, options.fasta_linewidth);
-        write_fasta_record("random_contig_rev", reverse_complement.as_slice(), output, options.fasta_linewidth);
+        write_fasta_record(
+            "random_contig",
+            subsequence,
+            &mut output,
+            options.fasta_linewidth,
+        );
+        write_fasta_record(
+            "random_contig_rev",
+            reverse_complement.as_slice(),
+            output,
+            options.fasta_linewidth,
+        );
     }
 
     println!("Done.");
@@ -49,16 +64,22 @@ fn generate(length: usize, rng: &mut impl Rng) -> Vec<u8> {
     let alphabet = [b'A', b'C', b'G', b'T'];
     let mut result = Vec::new();
 
-    let repetition_weights = (0..100).map(|r| if r == 0 {0.0} else {0.9f32.powi(r - 1)
-        + if r <= 1 {
-        2000.0
-    } else if r <= 4 {
-        40.0
-    } else if r <= 10 {
-        1.0
-    } else {
-        0.0
-    }});
+    let repetition_weights = (0..100).map(|r| {
+        if r == 0 {
+            0.0
+        } else {
+            0.9f32.powi(r - 1)
+                + if r <= 1 {
+                    2000.0
+                } else if r <= 4 {
+                    40.0
+                } else if r <= 10 {
+                    1.0
+                } else {
+                    0.0
+                }
+        }
+    });
     let repetition_distribution = WeightedIndex::new(repetition_weights).unwrap();
 
     while result.len() < length {
@@ -91,7 +112,12 @@ fn reverse_complement(forwards: &[u8]) -> Vec<u8> {
         .collect()
 }
 
-fn write_fasta_record(name: &str, sequence: &[u8], mut writer: impl Write, line_width: Option<usize>) {
+fn write_fasta_record(
+    name: &str,
+    sequence: &[u8],
+    mut writer: impl Write,
+    line_width: Option<usize>,
+) {
     writeln!(writer, ">{name}").unwrap();
     if let Some(line_width) = line_width {
         for chunk in sequence.chunks(line_width) {
